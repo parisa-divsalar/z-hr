1474044be35f5445bd79292ee844d93d02f27f15
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2025-11-27 10:03:26 +0330

    add Preview Edite

--- src/app/(private)/history-edite/tabs/SkillGapTabContent.tsx
+++ src/app/(private)/history-edite/tabs/SkillGapTabContent.tsx
@@ -26,3 +26,6 @@ const SkillGapTabContent = () => {
 };
 
 export default SkillGapTabContent;
+
+
+
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,21 +1,35 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Stack } from '@mui/material';
+import { useSearchParams } from 'next/navigation';
 
 import AIInput from '@/components/Landing/AI';
 import { AIStatus } from '@/components/Landing/type';
 import Wizard from '@/components/Landing/Wizard';
 
 export default function LandingPage() {
+  const searchParams = useSearchParams();
   const [aiStatus, setAiStatus] = useState<AIStatus>('START');
+  const [initialStep, setInitialStep] = useState<number>(1);
+
+  useEffect(() => {
+    const stepParam = searchParams.get('step');
+    if (stepParam) {
+      const step = parseInt(stepParam, 10);
+      if (!isNaN(step) && step >= 1 && step <= 3) {
+        setAiStatus('WIZARD');
+        setInitialStep(step);
+      }
+    }
+  }, [searchParams]);
 
   return (
     <Stack width='100%' height='100%'>
       {aiStatus === 'START' ? (
         <AIInput setAiStatus={setAiStatus} />
       ) : aiStatus === 'WIZARD' ? (
-        <Wizard setAiStatus={setAiStatus} />
+        <Wizard setAiStatus={setAiStatus} initialStep={initialStep} />
       ) : (
         <Stack />
       )}
