1190b513ec1bad7b14e79a444d442b08bec00475
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2025-12-02 23:20:18 +0330

    moved file ResumeBuilder

--- src/app/(private)/resume-builder/page.tsx
+++ src/app/(private)/resume-builder/page.tsx
@@ -6,6 +6,7 @@ import { Stack, Typography } from '@mui/material';
 
 import ResumeAIInput from '@/app/(private)/resume-builder/ResumeAIInput';
 import ResumeBuilderStep1 from '@/app/(private)/resume-builder/ResumeBuilderStep1';
+import StepWrapper from '@/components/Landing/Wizard/Stepper';
 import { AIStatus } from '@/components/Landing/type';
 
 import { ResumeBuilderRoot } from './styled';
@@ -19,17 +20,18 @@ const ResumeBuilder: FunctionComponent = () => {
       <Typography variant='h5' fontWeight='500' color='text.primary'>
         Resume Builder
       </Typography>
-      {aiStatus === 'START' ? (
-        <ResumeAIInput setAiStatus={setAiStatus} />
-      ) : aiStatus === 'WIZARD' ? (
-        activeStep === 1 ? (
-          <ResumeBuilderStep1 setAiStatus={setAiStatus} setActiveStep={setActiveStep} />
-        ) : (
-          <Stack />
-        )
-      ) : (
-        <Stack />
+      {aiStatus === 'START' && <ResumeAIInput setAiStatus={setAiStatus} />}
+
+      {aiStatus === 'WIZARD' && (
+        <Stack width='100%' height='100%' alignItems='center' p={5}>
+          <StepWrapper activeStep={activeStep} />
+
+          {activeStep === 1 && <ResumeBuilderStep1 setAiStatus={setAiStatus} setActiveStep={setActiveStep} />}
+          {activeStep !== 1 && <Stack />}
+        </Stack>
       )}
+
+      {aiStatus !== 'START' && aiStatus !== 'WIZARD' && <Stack />}
     </ResumeBuilderRoot>
   );
 };
