d2ed76518e0d4b5291d878d1b8de797c1f58b0f3
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2025-12-14 17:14:24 +0330

    update esume builder

--- src/app/(private)/resume-builder/page.tsx
+++ src/app/(private)/resume-builder/page.tsx
@@ -1,46 +1,19 @@
 'use client';
 
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
-import { Stack, Typography } from '@mui/material';
-
-import ResumeAIInput from '@/app/(private)/resume-builder/ResumeAIInput';
-import ResumeBuilderStep1 from '@/app/(private)/resume-builder/ResumeBuilderStep1';
-import ResumeBuilderStep2 from '@/app/(private)/resume-builder/ResumeBuilderStep2';
-import ResumeBuilderStep3 from '@/app/(private)/resume-builder/ResumeBuilderStep3';
-import { AIStatus } from '@/components/Landing/type';
+import { Typography } from '@mui/material';
 
 import { ResumeBuilderRoot } from './styled';
 
 const ResumeBuilder: FunctionComponent = () => {
-  const [aiStatus, setAiStatus] = useState<AIStatus>('START');
-  const [activeStep, setActiveStep] = useState<number>(1);
-
-  const renderStep = () => {
-    switch (activeStep) {
-      case 1:
-        return <ResumeBuilderStep1 setAiStatus={setAiStatus} setActiveStep={setActiveStep} />;
-      case 2:
-        return <ResumeBuilderStep2 setActiveStep={setActiveStep} />;
-      case 3:
-        return <ResumeBuilderStep3 setActiveStep={setActiveStep} />;
-      default:
-        return <Stack />;
-    }
-  };
-
-  return (
-    <ResumeBuilderRoot>
-      <Typography variant='h5' fontWeight='500' color='text.primary'>
-        Resume Builder
-      </Typography>
-      {aiStatus === 'START' && <ResumeAIInput setAiStatus={setAiStatus} />}
-
-      {aiStatus === 'WIZARD' && renderStep()}
-
-      {aiStatus !== 'START' && aiStatus !== 'WIZARD' && <Stack />}
-    </ResumeBuilderRoot>
-  );
+    return (
+        <ResumeBuilderRoot>
+            <Typography variant='h5' fontWeight='500' color='text.primary'>
+                Resume Builder
+            </Typography>
+        </ResumeBuilderRoot>
+    );
 };
 
 export default ResumeBuilder;
