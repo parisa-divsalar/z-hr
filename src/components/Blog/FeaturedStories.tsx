12e81aaa8fb03aec0117cc42a2c1ebe396758c9a
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2026-01-22 01:03:07 +0330

    add blog

--- src/components/Blog/FeaturedStories.tsx
+++ src/components/Blog/FeaturedStories.tsx
@@ -1,49 +1,12 @@
 'use client';
 
-import { Button, Stack, Typography } from '@mui/material';
-
 import ImageCardSlider from '@/components/Blog/ImageCardSlider';
 import { BlogSection } from '@/components/Blog/ImageCardSlider.styles';
 
 export default function FeaturedStories() {
     return (
-        <BlogSection
-            justifyContent='center'
-            alignItems='center'
-            width='100%'
-            textAlign='center'
-            gap={2}
-        >
-            <Typography variant='h2' color='secondary.main' fontWeight='700' pt={12}>
-                Blog{' '}
-            </Typography>
-
-            <Typography
-                variant='body1'
-                color='secondary.main'
-                fontWeight='492'
-                textAlign='center'
-            >
-                "Create a professional and ATS-friendly resume and CV in minutes with Z-CV.{' '}
-            </Typography>
-
-            <Typography
-                variant='body1'
-                color='secondary.main'
-                fontWeight='492'
-                textAlign='center'
-            >
-                Tailored for the markets of Iran and Dubai, featuring modern templates and
-                advanced artificial intelligence.{' '}
-            </Typography>
-
+        <BlogSection gap={2}>
             <ImageCardSlider />
-
-            <Stack direction='row' justifyContent='center' mt={5} mb={15}>
-                <Button variant='contained' color='secondary' size='medium'>
-                    View Archive{' '}
-                </Button>
-            </Stack>
         </BlogSection>
     );
 }
