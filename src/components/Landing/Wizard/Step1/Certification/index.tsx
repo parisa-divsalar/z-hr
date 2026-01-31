09b2439ae207b66a64877b0c3602701f3a4d24cb
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2026-01-31 18:35:44 +0330

    fix bug

--- src/components/Landing/Wizard/Step1/Certification/index.tsx
+++ src/components/Landing/Wizard/Step1/Certification/index.tsx
@@ -398,7 +398,8 @@ const Certification: FunctionComponent<CertificationProps> = ({ setStage }) => {
     };
 
     return (
-        <Stack alignItems='center' justifyContent='center'>
+            <Stack alignItems='center' justifyContent='flex-start' width='100%'>
+
             <Typography variant='h5' color='text.primary' fontWeight='584' mt={2}>
                 7. Capture your certifications
             </Typography>
