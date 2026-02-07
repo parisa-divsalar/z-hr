c01a0c21fde7287b02560e712a2653e194302eaa
parisaDivsalar <85540566+parisa-divsalar@users.noreply.github.com>
2026-02-07 15:34:22 +0330

    update   contact us page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
 import { Stack } from '@mui/material';
 import { usePathname } from 'next/navigation';
@@ -9,25 +9,42 @@ import classes from '@/components/Layout/layout.module.css';
 import Navbar from '@/components/Layout/Navbar';
 import SideBar from '@/components/Layout/SideBar';
 import AddToHomeScreen from '@/components/Other/AddToHomeScreen';
+import { normalizeRoute, PublicRoutes } from '@/config/routes';
 // import SplashScreen from '@/components/Other/SplashScreen';
 
 const Layout = ({ children }: { children: ReactNode }) => {
     const pathname = usePathname();
 
     const isHome = pathname === '/';
+    const normalizedPathname = normalizeRoute(pathname);
+    const hasPageFooter = normalizedPathname === PublicRoutes.contactUs;
 
     return (
         <Stack className={classes.mainLayout}>
-            <Stack className={classes.layoutContainer} bgcolor='background.default'>
+            <Stack
+                className={classes.layoutContainer}
+                bgcolor='background.default'
+                style={
+                    hasPageFooter
+                        ? ({
+                              ['--footer-height' as any]: '0px',
+                          } as CSSProperties)
+                        : undefined
+                }
+            >
                 <Navbar />
-                <Stack className={`${classes.childrenContainer} ${isHome ? classes.homeChildren : ''}`}>
+                <Stack
+                    className={`${classes.childrenContainer} ${isHome ? classes.homeChildren : ''} ${
+                        hasPageFooter ? classes.noBottomPadding : ''
+                    }`}
+                >
                     <Stack direction='row' className={classes.childrenInner}>
                         <SideBar />
 
                         <Stack className={classes.children}>{children}</Stack>
                     </Stack>
                 </Stack>
-                <Footer />
+                {!hasPageFooter && <Footer />}
             </Stack>
 
             {/*<SplashScreen />*/}
